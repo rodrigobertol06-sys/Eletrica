01f1cb9a1367146ac0bc85df4d8f0b3a0e1aa925
rodrigobertol06-sys <rodrigobertol06@gmail.com>
2026-07-22 21:33:31 -0300

    Criar sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('calc-eletrica-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/calc.js',
+        '/enciclopedia.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
